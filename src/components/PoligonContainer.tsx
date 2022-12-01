0cbc6fe1e3e5fcdebf354b281b2cddec950ff5b7
DrHeinz11 <franco.martin.1998m@gmail.com>
2022-12-01 19:33:12 -0300

    [fix] fix border glitch

--- src/components/PoligonContainer.tsx
+++ src/components/PoligonContainer.tsx
@@ -4,7 +4,7 @@ type poligonCta = { children: React.ReactNode, title: string, route: string, dir
 
 const PoligonContainer: FC<poligonCta> = ({ children, title, route, direction }) => {
     return (
-        <div className={`App w-[158px] h-[151px] ${direction === 'right' ? 'path-container' : 'path-container-left'} relative overflow-hidden bg-darkViolet rounded-[10px] border-[1px] border-darkViolet flex items-end justify-center p-3`}>
+        <div className={`App w-[158px] h-[151px] ${direction === 'right' ? 'path-container' : 'path-container-left'} relative overflow-hidden bg-darkViolet rounded-[10px]  flex items-end justify-center p-3`}>
             {children}
             <h1 className={`font-heading text-[#fff] text-2xl capitalize ${title=='cuenta'&&'relative -top-[15px]'}`}>{title}</h1>
         </div>
